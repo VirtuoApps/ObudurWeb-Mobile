628da1915894eaae6720eb9fa04ab11f7c2faa39
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-05-23 11:13:05 +0300

    Implement localization for post listing button in AuthBox
    - Updated AuthBox to use localized text for the "Post Listing" button, enhancing user experience for both English and Turkish languages.
    - Added "postListing" key to language files for English and Turkish to support the new localization feature.

--- app/HomePage/Header/AuthBox/AuthBox.tsx
+++ app/HomePage/Header/AuthBox/AuthBox.tsx
@@ -74,7 +74,7 @@ export default function AuthBox() {
                 }}
                 className="border border-[#D9D9D9] rounded-lg px-4 py-2 transition-all duration-300 hover:bg-[#362C75] hover:text-white text-[#5E5691] cursor-pointer"
               >
-                <p className="">İlan Ver</p>
+                <p className="">{t("postListing")}</p>
               </button>
             ))}
 
