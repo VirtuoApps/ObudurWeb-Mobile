4f7e3e733c11592d9f68f72442d5dd37da8c0e04
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-05-28 16:32:59 +0300

    Refactor MenuItems component to improve hover effects and text display
    - Changed the structure of MenuItems to use a div instead of a paragraph for better layout.
    - Enhanced hover effects by adding a secondary text display that transitions on hover.
    - Improved text color handling for active and hover states for better user feedback.

--- app/[locale]/resident/[slug]/Header/MenuItems/MenuItems.tsx
+++ app/[locale]/resident/[slug]/Header/MenuItems/MenuItems.tsx
@@ -77,17 +77,31 @@ export default function MenuItems() {
   return (
     <div className="flex md:flex-row flex-col w-full items-center gap-10 mb-20 md:mb-0">
       {menuItems.map((item) => (
-        <p
+        <div
           key={item.key}
-          className={`text-sm font-medium cursor-pointer hover:text-[#362C75] transition-all duration-200  ${
+          className={`text-sm font-medium cursor-pointer transition-all duration-200 overflow-hidden h-5 relative group ${
             activeSection === item.sectionId
-              ? " text-[#362C75]"
-              : "border-transparent text-[#8C8C8C] hover:text-[#8C8C8C]"
+              ? "text-[#362C75]"
+              : "text-[#8C8C8C]"
           }`}
           onClick={() => scrollToSection(item.sectionId)}
         >
-          {item.label}
-        </p>
+          {/* Normal text */}
+          <p
+            className={`transition-transform duration-300 ease-in-out group-hover:-translate-y-full ${
+              activeSection === item.sectionId
+                ? "text-[#362C75]"
+                : "text-[#8C8C8C]"
+            }`}
+          >
+            {item.label}
+          </p>
+
+          {/* Hover text (purple) */}
+          <p className="absolute top-0 left-0 text-[#362C75] translate-y-full transition-transform duration-300 ease-in-out group-hover:translate-y-0">
+            {item.label}
+          </p>
+        </div>
       ))}
     </div>
   );
