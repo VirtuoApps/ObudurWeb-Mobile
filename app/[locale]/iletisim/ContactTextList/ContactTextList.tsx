cbcd4be95019cf1c6b5754df45566a703ee59cf7
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-05-29 10:42:04 +0300

    Enhance AgreementsBox functionality with item selection and detail display
    - Implemented state management for selected items in AgreementsBox.
    - Updated LeftSide and Boxes components to handle item click events and pass selected item data.
    - Enhanced RightSide component to dynamically render details based on the selected item.
    - Improved content presentation by adding markdown text for cookie policy.

---
+++ app/[locale]/iletisim/ContactTextList/ContactTextList.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default function ContactTextList() {
+  return (
+    <div className=" px-6 py-12 md:py-16">
+      <div className="max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12">
+          {/* Merkez Ofis */}
+          <div>
+            <h3 className="text-[#262626] text-2xl font-semibold mb-6">
+              Merkez Ofis
+            </h3>
+            <div className="">
+              <p className="text-[#262626] text-base">
+                Havaalan Cad. Organize San. Sitesi 3M/C
+              </p>
+              <p className="text-[#262626] text-base">Muratpaşa / Antalya</p>
+              <a
+                href="#"
+                className="text-[#5E5691] text-base inline-block mt-3 hover:underline"
+              >
+                Yol Tarifi Al
+              </a>
+            </div>
+          </div>
+
+          {/* İletişim Kanalları */}
+          <div className="flex flex-col items-center ">
+            <h3 className="text-[#262626] text-2xl font-semibold mb-6">
+              İletişim Kanalları
+            </h3>
+            <div className="space-y-2">
+              <p className="text-[#262626] text-base text-center">
+                +90 312 345 67 89
+              </p>
+              <p className="text-[#262626] text-base text-center">
+                info@obudur.com
+              </p>
+              <p className="text-[#262626] text-base text-center">
+                kariyer@obudur.com
+              </p>
+            </div>
+          </div>
+
+          {/* Bizi Takip Edin */}
+          <div>
+            <h3 className="text-[#262626] text-2xl font-semibold mb-4 flex justify-end">
+              Bizi Takip Edin
+            </h3>
+            <div className="flex flex-col items-end">
+              <span className="inline-flex items-center  rounded-full text-[#262626] text-base">
+                Facebook
+              </span>
+              <span className="inline-flex items-center  rounded-full text-[#262626] text-base">
+                Instagram
+              </span>
+              <span className="inline-flex items-center  rounded-full text-[#262626] text-base">
+                LinkedIn
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/[locale]/iletisim/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Header from "./Header/Header";
+import ContactTextList from "./ContactTextList/ContactTextList";
+
+export default function contact() {
+  return (
+    <div className="bg-[#ebeaf1] w-full min-h-screen pt-20 px-2">
+      <Header />
+      <div className="flex flex-col items-center justify-center text-center py-10">
+        <h1 className=" sm:text-[56px] text-[32px] md:text-[56px] font-bold text-[#362C75] mb-3">
+          Obudur’a ulaşmak çok kolay!
+        </h1>
+        <p className="text-[16px] md:text-[24px] text-[#262626] mb-3  text-left">
+          Obudur, işini büyütmek isteyen bağımsız emlakçılara güvenilir bir
+          zemin sunar.
+        </p>
+        <p className="text-base text-[#595959] font-medium">
+          Eğitimden teknolojiye, pazarlamadan kurumsal kimliğe kadar her adımda
+          yanındayız.
+        </p>
+        <p className="text-base text-[#595959] font-medium">
+          Kendi ofisini açarken yalnız kalmak istemiyorsan, başvurunu
+          yap: aradığın o destek, O Budur.
+        </p>
+      </div>
+
+      <img
+        src="/contact.png"
+        alt="bayimiz"
+        className="w-full max-w-[1440px] mx-auto"
+      />
+
+      <ContactTextList />
+    </div>
+  );
+}
