4081b07bda2866aab73cd142dea10098d07b38a7
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-06-01 11:44:11 +0300

    Update CategorySelect component to increase minimum width for improved usability

--- app/HomePage/Header/MiddleSearchBox/CategorySelect/CategorySelect.tsx
+++ app/HomePage/Header/MiddleSearchBox/CategorySelect/CategorySelect.tsx
@@ -126,7 +126,7 @@ export default function CategorySelect({
       onSelect={handleCategorySelect}
       options={categories}
       defaultText={t("category")}
-      extraClassName="min-w-[150px] text-[#8c8c8c] hover:text-[#595959] transition-all duration-300"
+      extraClassName="min-w-[180px] text-[#8c8c8c] hover:text-[#595959] transition-all duration-300"
       customTextColor={true}
     />
   );
