fde71269617a5bfd2dc51864ce6f9740a02b9455
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-06-01 11:28:07 +0300

    Enhance Link component to prevent NProgress from starting on same page navigation
    - Integrated usePathname and useSearchParams hooks to determine the current URL.
    - Added logic to compare the current URL with the target href, preventing unnecessary NProgress starts for same-page links.
    - Improved handling of URLs by stripping language codes and hash fragments for accurate comparison.

--- app/components/nprogress/Link.tsx
+++ app/components/nprogress/Link.tsx
@@ -3,12 +3,16 @@
 import NextLink from "next/link";
 import { forwardRef } from "react";
 import NProgress from "nprogress";
+import { usePathname, useSearchParams } from "next/navigation";
 
 import { shouldTriggerStartEvent } from "./shouldTriggerStartEvent";
 
 export const Link = forwardRef<HTMLAnchorElement, React.ComponentProps<"a">>(
   function Link({ href, onClick, ...rest }, ref) {
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
     const useLink = href && href.toString().startsWith("/");
+
     if (!useLink)
       return (
         <a href={href?.toString()} onClick={onClick} {...rest} ref={ref} />
@@ -18,8 +22,37 @@ export const Link = forwardRef<HTMLAnchorElement, React.ComponentProps<"a">>(
       <NextLink
         href={href}
         onClick={(event) => {
-          if (shouldTriggerStartEvent(href.toString(), event))
+          let targetHref = href.toString();
+
+          // Create current URL for comparison
+          let currentUrl =
+            pathname +
+            (searchParams.toString() ? `?${searchParams.toString()}` : "");
+
+          //Remove /tr or /en from currentUrl
+          currentUrl = currentUrl.replace("/tr", "").replace("/en", "");
+
+          //Remove everyting after #
+          let targetHrefWithoutHash = targetHref;
+          if (targetHrefWithoutHash.includes("#")) {
+            targetHrefWithoutHash = targetHref.split("#")[0];
+          }
+
+          console.log({
+            currentUrl,
+            targetHref,
+          });
+
+          // Multiple checks to ensure we don't start progress for same page
+          const isSamePage =
+            currentUrl === targetHrefWithoutHash ||
+            pathname === targetHrefWithoutHash ||
+            !shouldTriggerStartEvent(targetHref, event);
+
+          if (!isSamePage) {
             NProgress.start();
+          }
+
           if (onClick) onClick(event);
         }}
         {...rest}
