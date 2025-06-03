7fec7f80b64c6f35096fc3e835db6d8dd1e9da33
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-06-03 12:23:38 +0300

    Enhance AuthBox component for improved mobile responsiveness and user experience
    - Updated dropdown menu layout to support mobile close button and headers for both mobile and desktop views.
    - Adjusted styling for links and buttons to enhance visibility and interaction on different screen sizes.
    - Added icons to menu items for better visual representation and user navigation.
    - Improved overall structure and responsiveness of AuthBox elements.

--- app/HomePage/Header/AuthBox/AuthBox.tsx
+++ app/HomePage/Header/AuthBox/AuthBox.tsx
@@ -167,8 +167,28 @@ export default function AuthBox({
 
         {/* Dropdown Menu */}
         {dropdownOpen && (
-          <div className="absolute lg:-right-4 right-0 mt-2 min-w-[320px]   bg-white rounded-[16px] shadow-lg z-50 py-1">
-            <div className="px-4  border-b border-gray-100 pb-3">
+          <div className="fixed lg:absolute inset-0 lg:inset-auto lg:-right-4 lg:right-0 lg:mt-2 lg:min-w-[320px] bg-white lg:rounded-[16px] lg:shadow-lg z-50 lg:py-1">
+            {/* Mobile Close Button */}
+
+            {/* Mobile Header */}
+            <div className="lg:hidden px-4 pb-6 flex flex-row items-center justify-between mt-5">
+              <div>
+                <p className="text-[#362C75] text-[24px] font-bold">
+                  {user.firstName} {user.lastName}
+                </p>
+                <p className="text-[#A39EC0] text-[16px]">{user.email}</p>
+              </div>
+
+              <button
+                onClick={() => setDropdownOpen(false)}
+                className="w-8 h-8 flex items-center justify-center rounded-full hover:bg-gray-100"
+              >
+                <img src="/popup-close-icon.png" className="w-6 h-6" />
+              </button>
+            </div>
+
+            {/* Desktop Header */}
+            <div className="hidden lg:block px-4 border-b border-gray-100 pb-3">
               <p className="text-[#362C75] text-[16px] font-bold">
                 {user.firstName} {user.lastName}
               </p>
@@ -177,13 +197,30 @@ export default function AuthBox({
 
             <Link
               href="/account"
-              className=" px-4 py-2 text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center"
+              className="px-4 py-4 lg:py-2 text-[16px] lg:text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center justify-between"
             >
-              <img
-                src="/user-profile-settings-2.png"
-                className="w-[20px] h-[20px] mr-2"
-              />
-              Profil Detayları
+              <div className="flex flex-row items-center font-medium lg:font-normal">
+                <img
+                  src="/user-profile-settings-2.png"
+                  className="w-[24px] h-[24px] lg:w-[20px] lg:h-[20px] mr-3 lg:mr-2 "
+                />
+                Profil Detayları
+              </div>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9 18L15 12L9 6"
+                  stroke="#595959"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  strokeLinejoin="round"
+                />
+              </svg>
             </Link>
 
             {/* <Link
@@ -202,24 +239,64 @@ export default function AuthBox({
               Mesajlar (2)
             </Link> */}
 
-            <div className="border-b border-gray-100 my-2"></div>
+            <div className="hidden lg:block border-b border-gray-100 my-3 lg:my-2"></div>
 
             {(user.role === "admin" || user.role === "super-admin") && (
               <Link
                 href="/admin/ilanlar"
-                className=" px-4 py-2 text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center"
+                className="px-4 py-4 lg:py-2 text-[16px] lg:text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center justify-between"
               >
-                <img src="/favourite.png" className="w-[20px] h-[20px] mr-2" />
-                İlanlarım
+                <div className="flex flex-row items-center font-medium lg:font-normal">
+                  <img
+                    src="/favourite.png"
+                    className="w-[24px] h-[24px] lg:w-[20px] lg:h-[20px] mr-3 lg:mr-2"
+                  />
+                  İlanlarım
+                </div>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9 18L15 12L9 6"
+                    stroke="#595959"
+                    strokeWidth="2"
+                    strokeLinecap="square"
+                    strokeLinejoin="round"
+                  />
+                </svg>
               </Link>
             )}
 
             <Link
               href="/favori-aramalar"
-              className=" px-4 py-2 text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center"
+              className="px-4 py-4 lg:py-2 text-[16px] lg:text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center justify-between"
             >
-              <img src="/heart.png" className="w-[20px] h-[20px] mr-2" />
-              Favori ilanlar
+              <div className="flex flex-row items-center font-medium lg:font-normal">
+                <img
+                  src="/heart.png"
+                  className="w-[24px] h-[24px] lg:w-[20px] lg:h-[20px] mr-3 lg:mr-2"
+                />
+                Favori ilanlar
+              </div>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9 18L15 12L9 6"
+                  stroke="#595959"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  strokeLinejoin="round"
+                />
+              </svg>
             </Link>
 
             {/* <Link
@@ -230,20 +307,86 @@ export default function AuthBox({
               Favori aramalar
             </Link> */}
 
-            <div className="border-b border-gray-100 my-2"></div>
+            <div className="hidden lg:block border-b border-gray-100 my-3 lg:my-2"></div>
 
             <Link
               href="/iletisim"
-              className=" px-4 py-2 text-[14px] text-[#595959]  hover:bg-gray-100 flex flex-row items-center"
+              className="px-4 py-4 lg:py-2 text-[16px] lg:text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center justify-between"
             >
-              İletişim Kanalları
+              <div className="flex flex-row items-center font-medium lg:font-normal">
+                <svg
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[24px] h-[24px] lg:w-[20px] lg:h-[20px] mr-3 lg:mr-2"
+                >
+                  <path
+                    d="M4.99983 11.5C4.99983 7.08172 8.58125 3.5 12.9992 3.5C17.4171 3.5 20.9985 7.08172 20.9985 11.5C20.9985 12.65 20.7558 13.7434 20.319 14.7316L21 19.4992L16.9146 18.4778C15.7572 19.1287 14.4215 19.5 12.9992 19.5M3.00095 16.5C3.00095 17.2188 3.15258 17.9021 3.4256 18.5198L3 21.4995L5.55315 20.8611C6.27643 21.268 7.11115 21.5 8.00005 21.5C10.761 21.5 12.9992 19.2614 12.9992 16.5C12.9992 13.7386 10.761 11.5 8.00005 11.5C5.23912 11.5 3.00095 13.7386 3.00095 16.5Z"
+                    stroke="#262626"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                İletişim Kanalları
+              </div>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9 18L15 12L9 6"
+                  stroke="#262626"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  strokeLinejoin="round"
+                />
+              </svg>
             </Link>
 
             <Link
               href="/iletisim#offices-section"
-              className=" px-4 py-2 text-[14px] text-[#595959]  hover:bg-gray-100 flex flex-row items-center"
+              className="px-4 py-4 lg:py-2 text-[16px] lg:text-[14px] text-[#262626] hover:bg-gray-100 flex flex-row items-center justify-between"
             >
-              Gayrimenkul Ofislerimiz
+              <div className="flex flex-row items-center font-medium lg:font-normal">
+                <svg
+                  width="24"
+                  height="25"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-[24px] h-[24px] lg:w-[20px] lg:h-[20px] mr-3 lg:mr-2"
+                >
+                  <path
+                    d="M7.5 17.5625H16.5M11.3046 3.71117L3.50457 8.98603C3.18802 9.2001 3 9.54666 3 9.91605V19.7882C3 20.7336 3.80589 21.5 4.8 21.5H19.2C20.1941 21.5 21 20.7336 21 19.7882V9.91605C21 9.54665 20.812 9.2001 20.4954 8.98603L12.6954 3.71117C12.2791 3.42961 11.7209 3.42961 11.3046 3.71117Z"
+                    stroke="#262626"
+                    strokeWidth="1.5"
+                    strokeLinecap="square"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                Gayrimenkul Ofislerimiz
+              </div>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9 18L15 12L9 6"
+                  stroke="#262626"
+                  strokeWidth="2"
+                  strokeLinecap="square"
+                  strokeLinejoin="round"
+                />
+              </svg>
             </Link>
 
             {user.role === "super-admin" && (
@@ -252,35 +395,50 @@ export default function AuthBox({
                   "accessToken"
                 )}`}
                 target="_blank"
-                className=" px-4 py-2 text-[14px] text-[#595959]  hover:bg-gray-100 flex flex-row items-center"
+                className="px-4 py-4 lg:py-2 text-[16px] lg:text-[14px] text-[#595959] hover:bg-gray-100 flex flex-row items-center justify-between"
               >
-                Admin Paneli
+                <span>Admin Paneli</span>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9 18L15 12L9 6"
+                    stroke="#595959"
+                    strokeWidth="2"
+                    strokeLinecap="square"
+                    strokeLinejoin="round"
+                  />
+                </svg>
               </a>
             )}
 
-            <div className="border-b border-gray-100 my-2"></div>
+            <div className="border-b border-gray-100 my-3 lg:my-2"></div>
 
             <Link
               href="/iletisim"
-              className=" px-4 py-2 text-[12px] font-[500] text-[#595959]  hover:bg-gray-100 flex flex-row items-center"
+              className="px-4 py-4 lg:py-2 text-[14px] lg:text-[12px] font-[500] text-[#595959] hover:bg-gray-100 flex flex-row items-center"
             >
               Geri Bildirim
             </Link>
 
             <Link
               href="/sozlesmeler?id=sozlesmeler&itemId=bireysel"
-              className=" px-4 py-2 text-[12px] font-[500] text-[#595959]  hover:bg-gray-100 flex flex-row items-center"
+              className="px-4 py-4 lg:py-2 text-[14px] lg:text-[12px] font-[500] text-[#595959] hover:bg-gray-100 flex flex-row items-center"
             >
               Kullanıcı Sözleşmeleri
             </Link>
 
-            <div className="border-b border-gray-100 my-2"></div>
+            <div className="border-b border-gray-100 my-3 lg:my-2"></div>
 
             <button
               onClick={handleLogout}
-              className="block w-full text-left px-4 py-2 text-[12px] text-[#EF1A28] hover:bg-gray-100 cursor-pointer"
+              className="block w-full text-left px-4 py-4 lg:py-2 text-[14px] lg:text-[12px] text-[#EF1A28] hover:bg-gray-100 cursor-pointer flex flex-row items-center justify-between"
             >
-              Çıkış Yap
+              <span>Çıkış Yap</span>
             </button>
           </div>
         )}
