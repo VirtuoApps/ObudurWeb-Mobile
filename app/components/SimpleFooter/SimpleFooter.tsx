6140c95340fb63e3875530611a606c5642395881
VirtuoApps <info@virtuoapps.com>
2025-06-09 23:34:05 +0300

    created by talya removed

--- app/components/SimpleFooter/SimpleFooter.tsx
+++ app/components/SimpleFooter/SimpleFooter.tsx
@@ -10,11 +10,6 @@ export default function SimpleFooter() {
             © 2025 Obudur
           </p>
         </div>
-
-        <div className="flex flex-row items-center">
-          <p className="font-medium text-[#595959] text-[14px]">Created by</p>
-          <p className="font-medium text-[#8C8C8C] text-[14px] ml-1"> Talya</p>
-        </div>
       </div>
       <div className="w-full h-[16px] bg-[#C1BED4]"></div>
     </>
