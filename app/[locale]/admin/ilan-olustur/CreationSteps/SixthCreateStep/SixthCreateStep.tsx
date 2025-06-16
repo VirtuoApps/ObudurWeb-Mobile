89e3c6a44aa2d68cfb9aeaefed8de5872a1af554
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-06-16 20:28:53 +0300

    minor fix

--- app/[locale]/admin/ilan-olustur/CreationSteps/SixthCreateStep/SixthCreateStep.tsx
+++ app/[locale]/admin/ilan-olustur/CreationSteps/SixthCreateStep/SixthCreateStep.tsx
@@ -388,19 +388,12 @@ export default function SixthCreateStep() {
                 ekleyebilirsiniz.
               </p>
             </div>
-            <GoBackButton
-              handleBack={() => {
-                setCurrentStep(5);
-              }}
-              step={6}
-              totalSteps={6}
-            />
           </div>
 
           {/* Right Form Panel - 70% width on desktop */}
           <div
             ref={formPanelRef}
-            className="w-full md:w-[70%] md:pl-6 md:h-[80vh]  overflow-y-auto overflow-x-hidden"
+            className="w-full md:w-[70%] md:pl-6 md:h-[67vh]  2xl:h-[73vh]  overflow-y-auto overflow-x-hidden"
           >
             {/* Errors display */}
             {errors.length > 0 && (
@@ -624,46 +617,53 @@ export default function SixthCreateStep() {
             })}
 
             {/* Navigation buttons */}
-            <div className="mt-10 flex justify-end items-center">
-              <button
-                type="button"
-                onClick={handleComplete}
-                disabled={
-                  Object.values(uploadingDocs).some((uploading) => uploading) ||
-                  isSubmitting
-                }
-                className="bg-[#6656AD] hover:bg-[#5349a0] text-white font-semibold px-8 py-3 rounded-xl transition disabled:opacity-70 disabled:cursor-not-allowed"
-              >
-                {isSubmitting ? (
-                  <div className="flex items-center">
-                    <svg
-                      className="animate-spin mr-2 h-4 w-4 text-white"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                    >
-                      <circle
-                        className="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        strokeWidth="4"
-                      ></circle>
-                      <path
-                        className="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                      ></path>
-                    </svg>
-                    <span>İşleniyor...</span>
-                  </div>
-                ) : (
-                  <span>{isUpdate ? "Güncelle" : "Oluştur"}</span>
-                )}
-              </button>
-            </div>
           </div>
+        </div>
+        <div className=" flex flex-col sm:flex-row justify-between items-center p-6">
+          <GoBackButton
+            handleBack={() => {
+              setCurrentStep(5);
+            }}
+            step={6}
+            totalSteps={6}
+          />
+          <button
+            type="button"
+            onClick={handleComplete}
+            disabled={
+              Object.values(uploadingDocs).some((uploading) => uploading) ||
+              isSubmitting
+            }
+            className="bg-[#6656AD] hover:bg-[#5349a0] text-white font-semibold px-8 py-3 rounded-xl transition disabled:opacity-70 disabled:cursor-not-allowed"
+          >
+            {isSubmitting ? (
+              <div className="flex items-center">
+                <svg
+                  className="animate-spin mr-2 h-4 w-4 text-white"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                  ></path>
+                </svg>
+                <span>İşleniyor...</span>
+              </div>
+            ) : (
+              <span>{isUpdate ? "Güncelle" : "Oluştur"}</span>
+            )}
+          </button>
         </div>
       </div>
     </div>
