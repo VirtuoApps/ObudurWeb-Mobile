cd0cb35491e014ab0722a2e3466dfa7d9ef071de
Lepuz-coder <58707475+Lepuz-coder@users.noreply.github.com>
2025-06-26 20:12:14 +0300

    minor fix

--- app/[locale]/admin/ilan-olustur/CreationSteps/FourthCreateStep/FourthCreateStep.tsx
+++ app/[locale]/admin/ilan-olustur/CreationSteps/FourthCreateStep/FourthCreateStep.tsx
@@ -465,7 +465,7 @@ export default function FourthCreateStep() {
   }, []); // Empty dependency array means this runs once when component mounts
 
   return (
-    <div className="min-h-screen bg-[#ECEBF4] flex justify-center items-start p-4">
+    <div className=" bg-[#ECEBF4] flex justify-center items-start p-4">
       <div className="w-full max-w-[1200px] rounded-2xl shadow-lg bg-white">
         <div className="flex flex-col md:flex-row p-10">
           {/* Left Info Panel */}
